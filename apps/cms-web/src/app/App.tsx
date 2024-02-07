e657be755a8d36657c6ae2d89a34c766f8a303af
Krzysztof Antczak <a.krzychu@gmail.com>
2024-02-07 20:17:08 +0100

    [#DC1ZzcNO] App added - cms-web;

---
+++ apps/cms-web/src/app/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.scss'
+
+export function App() {
+    return (
+        <div className={styles.test}>
+            <h1 className={styles.bla}>
+                <span> Hello there, </span>
+                Welcome cms-web! 👋
+            </h1>
+        </div>
+    )
+}
+
+export default App
